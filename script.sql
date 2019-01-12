23157fae7e8f6b019b1a31773eb558141399c0fd
Kelvin Ashu <kelvin@pinevalleyinstitute.co.za>
2019-01-12 21:37:04 +0200

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `crossriver` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_read` datetime DEFAULT NULL,
+  `date_sent` datetime NOT NULL,
+  `is_read` bit(1) DEFAULT b'0',
+  `message` longtext NOT NULL,
+  `sent_from` varchar(45) NOT NULL,
+  `sent_to` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
